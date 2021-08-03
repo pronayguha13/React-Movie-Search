efb53da1ff29fc4273f8eb99212f9eb216c846e9
pronayguha13 <pronayguha13@gmail.com>
2021-08-04 01:48:18 +0530

    basic set up finished

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
-import React, { useState } from "react";
+import React from "react";
 
 import "./App.css";
 
 const App = () => {
-  const [movies, setMovies] = useState([]);
   return <div className="App">This is the App.</div>;
 };
 
